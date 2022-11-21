11090383d5548ec79cd28a64dd55c3105abadb51
SERUSEcode <kevin@ullbrandt.com>
2022-11-21 15:52:09 +0100

    Connection between backend and frontend all work

---
+++ TestAppRazorMAUI/TestAppRazorMAUI/obj/Debug/net6.0-android/android/src/crc64e6c08b24330e2696/MainApplication.java
@@ -0,0 +1,34 @@
+package crc64e6c08b24330e2696;
+
+
+public class MainApplication
+	extends crc6488302ad6e9e4df1a.MauiApplication
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+	}
+
+	public MainApplication ()
+	{
+		mono.MonoPackageManager.setContext (this);
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
